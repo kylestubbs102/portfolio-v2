9d56171831f01b03caceb5ccfc4676b65f78822e
Kyle Stubbs <kylestubbs@ucsb.edu>
2022-11-21 00:03:20 -0800

    added home page, added font awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
-import type { AppProps } from 'next/app'
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,3 +1,51 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import styles from "../../styles/Home.module.css";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.blackRectangle}>
+        <div className={styles.linkOuterContainer}>
+          <div className={styles.linkInnerContainer}>
+            <a
+              href="https://www.linkedin.com/in/kylestubbs102/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+            </a>
+            <a
+              href="https://github.com/kylestubbs102/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+            </a>
+            <a
+              href="mailto:kylestubbs102@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className={styles.diagonalBox} />
+      <div className={styles.whiteRectangle}>
+        <div className={styles.infoContainer}>
+          <p className={styles.introText}>Hello I&apos;m</p>
+          <p className={styles.nameText}>Kyle Stubbs</p>
+          <p className={styles.occupationText}>Software Engineer</p>
+          <p className={styles.descriptionText}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et dui,
+            ullamcorper ut quis congue purus volutpat posuere lorem.
+          </p>
+          <button className={styles.hireBtn}>About me</button>
+        </div>
+      </div>
+    </div>
+  );
 }

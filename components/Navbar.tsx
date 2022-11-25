f775a42cce3a24e5161b036e9c6ca19a7b31cc24
Kyle Stubbs <kylestubbs@ucsb.edu>
2022-11-25 02:49:39 -0800

    fix navbar css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,43 +1,112 @@
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { useState } from "react";
-import styles from "../styles/components/Navbar.module.css"
+import styles from "../styles/components/Navbar.module.css";
 
 export default function Navbar() {
+  const router = useRouter();
+
   const [click, setClick] = useState(false);
   const handleClick = () => setClick(!click);
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.navContainer}>
-        <Link href="/home" className={styles.navLogo}>
-          Kyle Stubbs
+        <Link href="/home" className={styles.navLogo} onClick={handleClick}>
+          KS
         </Link>
 
-        <ul className={click ? `${styles.navMenu} ${styles.active}` : styles.navMenu}>
+        <ul
+          className={
+            click ? `${styles.navMenu} ${styles.active}` : styles.navMenu
+          }
+        >
           <li className={styles.navItem}>
-            <Link href="/home" className={styles.navLinks}>Home</Link>
+            <Link
+              href="/home"
+              className={
+                router.pathname == "/home"
+                  ? `${styles.navLinks} ${styles.active}`
+                  : styles.navLinks
+              }
+              onClick={handleClick}
+            >
+              Home
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/about" className={styles.navLinks}>About</Link>
+            <Link
+              href="/about"
+              className={
+                router.pathname == "/about"
+                  ? `${styles.navLinks} ${styles.active}`
+                  : styles.navLinks
+              }
+              onClick={handleClick}
+            >
+              About
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/projects" className={styles.navLinks}>Projects</Link>
+            <Link
+              href="/projects"
+              className={
+                router.pathname == "/projects"
+                  ? `${styles.navLinks} ${styles.active}`
+                  : styles.navLinks
+              }
+              onClick={handleClick}
+            >
+              Projects
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/skills" className={styles.navLinks}>Skills</Link>
+            <Link
+              href="/skills"
+              className={
+                router.pathname == "/skills"
+                  ? `${styles.navLinks} ${styles.active}`
+                  : styles.navLinks
+              }
+              onClick={handleClick}
+            >
+              Skills
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/experience" className={styles.navLinks}>Experience</Link>
+            <Link
+              href="/experience"
+              className={
+                router.pathname == "/experience"
+                  ? `${styles.navLinks} ${styles.active}`
+                  : styles.navLinks
+              }
+              onClick={handleClick}
+            >
+              Experience
+            </Link>
           </li>
           <li className={styles.navItem}>
-            <Link href="/contact" className={styles.navLinks}>Contact</Link>
+            <Link
+              href="/contact"
+              className={
+                router.pathname == "/contact"
+                  ? `${styles.navLinks} ${styles.active}`
+                  : styles.navLinks
+              }
+              onClick={handleClick}
+            >
+              Contact
+            </Link>
           </li>
         </ul>
         <div className={styles.navIcon} onClick={handleClick}>
-            <FontAwesomeIcon icon={click ? faTimes : faBars} className={styles.icon}/>
-          {/* <i className={click ? "fa fa-times" : "fas fa-bars"}></i> */}
+          <FontAwesomeIcon
+            icon={click ? faTimes : faBars}
+            className={styles.icon}
+          />
         </div>
       </div>
     </nav>

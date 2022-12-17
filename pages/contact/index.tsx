18cd9c1ea621fb15d1db6f54296ddb23819814df
Kyle Stubbs <kylestubbs@ucsb.edu>
2022-12-17 04:18:30 -0800

    fixed contact page, got rid of box, made responsive

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -74,77 +74,58 @@ export default function Contact() {
         <p className={styles.title}>Contact</p>
         <Divider />
       </div>
-      <div className={styles.contentContainer}>
-        <div className={styles.dialogBox}>
-          <div className={styles.dialogContainer}>
-            <p className={styles.dialogTitle}>I&apos;d love to hear from you</p>
-            <p className={styles.dialogDescription}>
-              Feel free to contact me by submitting the form below, or send me
-              an email at{" "}
-              <span style={{ color: "red", textDecoration: "underline" }}>
-                kylestubbs102@gmail.com
-              </span>
-              .
-            </p>
-            <p className={styles.dialogDescription}>
-              You can also reach me at the following links.
-            </p>
-            <div className={styles.linkOuterContainer}>
-              <div className={styles.linkInnerContainer}>
-                <a
-                  href="https://www.linkedin.com/in/kylestubbs102/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
-                </a>
-                <a
-                  href="https://github.com/kylestubbs102/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faGithub} className={styles.icon} />
-                </a>
-                <a
-                  href="mailto:kylestubbs102@gmail.com"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
-                </a>
-              </div>
+      <form className={styles.inputContainer} onSubmit={(e) => handleSubmit(e)}>
+        <input
+          className={`${styles.baseInput} ${styles.normalInput}`}
+          ref={nameRef}
+          placeholder="Name"
+        />
+        <input
+          className={`${styles.baseInput} ${styles.normalInput}`}
+          ref={emailRef}
+          placeholder="Email"
+        />
+        <input
+          className={`${styles.baseInput} ${styles.normalInput}`}
+          ref={subjectRef}
+          placeholder="Subject"
+        />
+        <textarea
+          className={`${styles.baseInput} ${styles.textarea}`}
+          ref={messageRef}
+          placeholder="Message"
+        />
+        <div className={styles.bottomContainer}>
+          <div className={styles.linkOuterContainer}>
+            <div className={styles.linkInnerContainer}>
+              <a
+                href="https://www.linkedin.com/in/kylestubbs102/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faLinkedin} className={styles.icon} />
+              </a>
+              <a
+                href="https://github.com/kylestubbs102/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} className={styles.icon} />
+              </a>
+              <a
+                href="mailto:kylestubbs102@gmail.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FontAwesomeIcon icon={faEnvelope} className={styles.icon} />
+              </a>
             </div>
           </div>
-        </div>
-        <form
-          className={styles.inputContainer}
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          <input
-            className={`${styles.baseInput} ${styles.normalInput}`}
-            ref={nameRef}
-            placeholder="Name"
-          />
-          <input
-            className={`${styles.baseInput} ${styles.normalInput}`}
-            ref={emailRef}
-            placeholder="Email"
-          />
-          <input
-            className={`${styles.baseInput} ${styles.normalInput}`}
-            ref={subjectRef}
-            placeholder="Subject"
-          />
-          <textarea
-            className={`${styles.baseInput} ${styles.textarea}`}
-            ref={messageRef}
-            placeholder="Message"
-          />
           <button className={styles.button} type="submit">
             Send
           </button>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 }

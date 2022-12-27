d69d2f46c7525aa16196df6424d3f76974825251
Kyle Stubbs <kylestubbs@ucsb.edu>
2022-12-27 00:39:52 -0800

    Added hover effect to skill boxes

--- components/SkillBox.tsx
+++ components/SkillBox.tsx
@@ -1,5 +1,6 @@
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
 import styles from "../styles/components/Box.module.css";
 
 export interface SkillBoxData {
@@ -8,14 +9,55 @@ export interface SkillBoxData {
   icon: IconProp;
 }
 
+const boxMotion = {
+  rest: {
+    backgroundColor: "#FFFFFF",
+  },
+  hover: {
+    backgroundColor: "#FF0000",
+    scale: 1.05,
+    transition: { duration: 0.2, ease: "circOut" },
+  },
+};
+
+const iconMotion = {
+  rest: {
+    backgroundColor: "#000000",
+  },
+  hover: {
+    backgroundColor: "#FFFFFF",
+    transition: { duration: 0.1, ease: "circOut" },
+  },
+};
+
+const textMotion = {
+  rest: {
+    color: "#000000",
+  },
+  hover: {
+    color: "#FFFFFF",
+    transition: { duration: 0.1, ease: "circOut" },
+  },
+};
+
 export default function SkillBox({ title, description, icon }: SkillBoxData) {
   return (
-    <div className={styles.skillBox}>
-      <div className={styles.iconBox}>
+    <motion.div
+      initial="rest"
+      whileHover="hover"
+      animate="rest"
+      variants={boxMotion}
+      className={styles.skillBox}
+    >
+      <motion.div variants={iconMotion} className={styles.iconBox}>
         <FontAwesomeIcon icon={icon} className={styles.icon} />
-      </div>
-      <p className={styles.title}>{title}</p>
-      <p className={styles.description}>{description}</p>
-    </div>
+      </motion.div>
+      <motion.p variants={textMotion} className={styles.title}>
+        {title}
+      </motion.p>
+      <motion.p variants={textMotion} className={styles.description}>
+        {description}
+      </motion.p>
+    </motion.div>
   );
 }

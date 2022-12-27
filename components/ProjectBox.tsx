1c676ee01e60f9032ee0c9e1ff9eb324e2da97d8
Kyle Stubbs <kylestubbs@ucsb.edu>
2022-12-27 02:14:52 -0800

    Added hover effect to project boxes

--- components/ProjectBox.tsx
+++ components/ProjectBox.tsx
@@ -1,3 +1,6 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import pic from "../public/ProfilePicAbout.jpg";
 import styles from "../styles/components/Box.module.css";
 
 export interface ProjectBoxData {
@@ -5,11 +8,43 @@ export interface ProjectBoxData {
   description: string;
 }
 
+const infoMotion = {
+  rest: {
+    opacity: 1,
+  },
+  hover: {
+    opacity: 0,
+    transition: { duration: 0.2, ease: "easeOut" },
+  },
+};
+
+const imageMotion = {
+  rest: {
+    opacity: 0,
+  },
+  hover: {
+    opacity: 1,
+    transition: { duration: 0.2, ease: "easeOut" },
+  },
+};
+
+// TODO : fix styling, add links and images
 export default function ProjectBox({ title, description }: ProjectBoxData) {
   return (
-    <div className={styles.projectBox}>
-      <p className={styles.title}>{title}</p>
-      <p className={styles.description}>{description}</p>
-    </div>
+    <motion.a
+      initial="rest"
+      whileHover="hover"
+      className={styles.wrapper}
+      href="google.com"
+      target="_blank"
+    >
+      <motion.div variants={imageMotion}>
+        <Image src={pic} alt={title} fill />
+      </motion.div>
+      <motion.div variants={infoMotion} className={styles.projectBox}>
+        <p className={styles.title}>{title}</p>
+        <p className={styles.description}>{description}</p>
+      </motion.div>
+    </motion.a>
   );
 }

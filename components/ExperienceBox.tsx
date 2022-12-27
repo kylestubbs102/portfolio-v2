dd39ecde868f1a6f1a20afbbcc20e8f72ce69b67
Kyle Stubbs <kylestubbs@ucsb.edu>
2022-12-27 01:23:05 -0800

    Added hover effect to experience boxes

--- components/ExperienceBox.tsx
+++ components/ExperienceBox.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import styles from "../styles/components/Box.module.css";
 
 export interface ExperienceBoxData {
@@ -5,14 +6,45 @@ export interface ExperienceBoxData {
   description: string;
 }
 
+const boxMotion = {
+  rest: {
+    backgroundColor: "#FFFFFF",
+  },
+  hover: {
+    backgroundColor: "#FF0000",
+    scale: 1.05,
+    transition: { duration: 0.2, ease: "circOut" },
+  },
+};
+
+const textMotion = {
+  rest: {
+    color: "#000000",
+  },
+  hover: {
+    color: "#FFFFFF",
+    transition: { duration: 0.1, ease: "circOut" },
+  },
+};
+
 export default function ExperienceBox({
   title,
   description,
 }: ExperienceBoxData) {
   return (
-    <div className={styles.experienceBox}>
-      <p className={styles.title}>{title}</p>
-      <p className={styles.description}>{description}</p>
-    </div>
+    <motion.div
+      initial="rest"
+      whileHover="hover"
+      animate="rest"
+      variants={boxMotion}
+      className={styles.experienceBox}
+    >
+      <motion.p variants={textMotion} className={styles.title}>
+        {title}
+      </motion.p>
+      <motion.p variants={textMotion} className={styles.description}>
+        {description}
+      </motion.p>
+    </motion.div>
   );
 }
